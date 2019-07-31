0d5ce26a641833a0dbcad480b6365ee8792d2902
Brittany Javalera <brittjavalera@gmail.com>
2019-07-31 02:18:11 +0000

    first commit

---
+++ dog_hikes_cli/lib/dog_hikes_cli.rb
@@ -0,0 +1,6 @@
+require "dog_hikes_cli/version"
+
+module DogHikesCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ dog_hikes_cli/lib/dog_hikes_cli/version.rb
@@ -0,0 +1,3 @@
+module DogHikesCli
+  VERSION = "0.1.0"
+end
